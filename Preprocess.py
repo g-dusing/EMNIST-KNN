5a444d701ed76755ca2e13f638e54689b1bc303b
g-dusing <76131429+g-dusing@users.noreply.github.com>
2023-04-27 13:39:03 -0500

    Update Preprocess.py

--- Preprocess.py
+++ Preprocess.py
@@ -12,14 +12,25 @@ def preprocess(s="letters"):
     '''
     train_x, train_y = extract_training_samples(s)
     test_x, test_y = extract_test_samples(s)
-    train_x = train_x.reshape(124800, 784)
-    flat = []
+    # create training dataframe
+    train_shape = train_x.shape
+    train_x = train_x.reshape(train_shape[0], train_shape[1] * train_shape[2])
+    flat_train = []
     for i in train_x:
-        flat.append(i.flatten())
-    flat = np.array(flat)
-    df = pd.DataFrame(flat)
-    df["Label"] = train_y
-    return df
+        flat_train.append(i.flatten())
+    flat_train = np.array(flat_train)
+    df_train = pd.DataFrame(flat_train)
+    df_train["Label"] = train_y
+    # create testing dataframe
+    test_shape = test_x.shape
+    test_x = test_x.reshape(test_shape[0], test_shape[1] * test_shape[2])
+    flat_test = []
+    for j in test_x:
+        flat_test.append(j.flatten())
+    flat_test = np.array(flat_test)
+    df_test = pd.DataFrame(flat_test)
+    df_test["Label"] = test_y
+    return df_train, df_test
 
 
 if __name__ == "__main__":
